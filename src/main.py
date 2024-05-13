04ec350003bff9cb4bc2dab6628341693dd14eb0
Daniil Kudriashov <daniil.kudriashov@etu.unistra.fr>
2024-05-13 07:18:13 +0200

    added nb thread sytem detection

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from backend import *
+import os
 
 def main():
     # Execution
@@ -6,8 +7,9 @@ def main():
     # get organisms from path
     organisms = get_organisms_from_path("C:/Educ/Analyse_texte/new_proj/Results/Eukaryota/Animals/Mammals/Hippopotamus amphibius")
     print(organisms)
+    max_workers = os.cpu_count() + 2
     organisms = fetch_nc_ids_for_organisms(organisms)
-    records = fetch_genbank_records(organisms, rettype="gbwithparts")
+    records = fetch_genbank_records(organisms, rettype="gbwithparts", max_workers=max_workers)
     print("Records fetched. Extracting...")
     process_genomic_data(records, ["CDS", "intron", "mRNA"])
     # extract_and_save_regions(records)
